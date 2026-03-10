6d603a186492eac7bd7da158c5109bfd2f60683f
Frederik Born <macbook@MacBook.fritz.box>
2026-03-10 19:38:13 +0100

    add appname, input field, comfirm button

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
 import './App.css'
 
+
 function App() {
-  
+
+  return (
+    <div>
+      <h1>QuickWater</h1>
+      <div>
+
+        <input className='water-input'
+        type='number'
+        placeholder='Wasser(ml)'
+        />
+        <button className='water-save-button'>Getrunken</button>
+
+      </div> 
+    </div>
+  )
+
 }
 
 export default App
